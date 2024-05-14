55dec01a08138e0b7a38c55a0fa5424a96d50994
GiokyUnder <qgiok125@gmail.com>
2024-05-14 22:19:57 +0400

    add layout

---
+++ src/pages/models/data.js
@@ -0,0 +1,202 @@
+import aboutImg from "../../assets/img/about-red.jpg";
+
+export const models = [
+  {
+    params: { id: 1 },
+    props: {
+      name: "Аннет",
+      price: "50.000",
+      age: "23",
+      height: "172",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 2 },
+    props: {
+      name: "Мария",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 3 },
+    props: {
+      name: "Нени",
+      price: "90.000",
+      age: "18",
+      height: "165",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 4 },
+    props: {
+      name: "Челси",
+      price: "50.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 5 },
+    props: {
+      name: "Аня",
+      price: "60.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 6 },
+    props: {
+      name: "Агнес",
+      price: "65.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 7 },
+    props: {
+      name: "Ольга",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 8 },
+    props: {
+      name: "Жизель",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 9 },
+    props: {
+      name: "Лия",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 10 },
+    props: {
+      name: "Рина",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 11 },
+    props: {
+      name: "Марго",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 12 },
+    props: {
+      name: "Мила",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 13 },
+    props: {
+      name: "Аннет",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 14 },
+    props: {
+      name: "Инга",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 15 },
+    props: {
+      name: "Лила",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 16 },
+    props: {
+      name: "Мария",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 17 },
+    props: {
+      name: "Нести",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+  {
+    params: { id: 18 },
+    props: {
+      name: "Жанна",
+      price: "70.000",
+      age: "19",
+      height: "168",
+      img: aboutImg,
+      about: "люблю спорт",
+    },
+  },
+];

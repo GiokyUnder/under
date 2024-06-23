52da6977ab06c126e44ca657e69361246551f0f5
GiokyUnder <qgiok125@gmail.com>
2024-06-24 03:27:29 +0400

    redesign

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from "astro/config";
-
 import purgecss from "astro-purgecss";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [purgecss({
-    fontFace: true,
-    keyframes: true,
-    variables: true
-  })]
+  prefetch: {
+    prefetchAll: true,
+  },
+  integrations: [
+    purgecss({
+      fontFace: true,
+      keyframes: true,
+      variables: true,
+    }),
+    tailwind(),
+  ],
 });
